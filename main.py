707e6b064b5d2fdcab0cd11e0da7760e523b8b60
Claudio Vasquez <claudio.vasquez@unah.hn>
2024-01-12 01:13:47 -0600

    Done

--- main.py
+++ main.py
@@ -27,16 +27,58 @@ def pdf_to_xlsx(pdf_file_name, xlsx_file_name):
         df = table.df
         rows_to_delete = []
 
-        if "\nActividad:" in df.iloc[1, 1]:
-            # Significa que la actividad y el codigo de actividad estan juntos y hay que separarlos
+        linea1 = df.iloc[0].values
+        linea2 = df.iloc[1].values
+        linea3 = df.iloc[2].values
 
-            actividad = df.iloc[1, 1].split("\n")[1]
-            codigo_actividad = df.iloc[1, 1].split("\n")[0]
-            nombre_ficha = df.iloc[1, 2]
-            ##Replace the values in the dataframe on the 1,1  1,2 and 1,3 positions
-            df.iloc[1, 1] = codigo_actividad
-            df.iloc[1, 2] = actividad
-            df.iloc[1, 3] = nombre_ficha
+        if "Dirección de Proyectos" in linea1:
+            rows_to_delete.append(0)
+        if "Unidad de Costos" in linea2:
+            rows_to_delete.append(1)
+        if "Reporte De Fichas" in linea3:
+            rows_to_delete.append(2)
+        if "Reporte De Fichas" in linea1:
+            rows_to_delete.append(0)
+
+        df = df.drop(rows_to_delete)
+        df = df.reset_index(drop=True)
+
+        rows_to_delete = []
+
+        if "\nActividad:" in df.iloc[0, 1]:
+            df.iloc[0, 1] = df.iloc[0, 1].replace("\nActividad:", "")
+
+        if "Actividad:" in df.iloc[0, 2]:
+            df.iloc[0, 2] = df.iloc[0, 2].replace("Actividad:", "")
+            NombreFichaFinal = df.iloc[0, 3]
+            print("\033[94mNombre de ficha esta en la posicion 3\033[0m")
+            # This print statement should display the message "Nombre de ficha esta en la posicion 3" in the console with the color blue (94).
+            print("Nombre Ficha Final =", NombreFichaFinal)
+        elif df.iloc[0, 2] == "":
+            NombreFichaFinal = df.iloc[0, 3]
+            # This print statement should display the message "Nombre de ficha esta en la posicion 3" in the console with the color yellow (93).
+            print("\033[93mNombre de ficha esta en la posicion 3\033[0m")
+            print("Nombre Ficha Final =", NombreFichaFinal)
+        elif df.iloc[0, 2] != "":
+            NombreFichaFinal = df.iloc[0, 2]
+            # This print statement should display the message "Nombre de ficha esta en la posicion 2" in the console with the color red (91).
+            print("Nombre Ficha Final =", NombreFichaFinal)
+        else:
+            print("\033[91mError en el archivo: \033[0m", pdf_file_name)
+            print("Error: ", e)
+            print(df)
+            return
+
+        valor_CodigoActividad = df.iloc[0, 1]
+        print("Codigo Actividad =", valor_CodigoActividad)
+
+        for col in df.columns:
+            df.iloc[0, col] = ""
+
+        df.iloc[0, 0] = valor_CodigoActividad
+        df.iloc[0, 1] = NombreFichaFinal
+
+        # print(df.head(4))
 
         ##Revisa si la fila esta vacia y solo tiene un valor en el nombre del insumo, si es asi, lo agrega al nombree dle insumo anterior y elimina la fila que no tenia nada mas que eso
 
@@ -48,8 +90,8 @@ def pdf_to_xlsx(pdf_file_name, xlsx_file_name):
                     & (df.iloc[index, 3] == "")
                     & (df.iloc[index, 4] == "")
                     & (df.iloc[index, 5] == "")
-                    & (df.iloc[index, 6] == "")
                 ):
+                    print("Condicion vacia cumplida")
                     text = df.iloc[index, 1]
                     df.iloc[index - 1, 1] = df.iloc[index - 1, 1] + " " + text
                     rows_to_delete.append(index)
@@ -62,37 +104,39 @@ def pdf_to_xlsx(pdf_file_name, xlsx_file_name):
 
         df = df.drop(rows_to_delete)
 
+        print("\n\n")
+
         df.to_excel(xlsx_file_name, index=False)
 
 
 def main():
     path = "Fichas1.pdf"
 
-    # split_pdf(path)
+    split_pdf(path)
 
     files = os.listdir("output/Temp")
     pdf_files = [file for file in files if file.endswith(".pdf")]
 
     print(f"Total number of PDF files: {len(pdf_files)}")
-    
 
     for i in range(1, len(pdf_files) + 1):
-        # Your code here
-        # Replace '...' with the code you want to execute for each iteration
-        pdf_filename= f"output/Temp/Fichas1_page_{i}.pdf"
+    # for i in range(1, 200):
+
+        pdf_filename = f"output/Temp/Fichas1_page_{i}.pdf"
         xlsx_output_filename = f"output/Xlsx/Fichas1_page_{i}.xlsx"
+        print("\n\n")
+
         print(f"Converting {pdf_filename} to {xlsx_output_filename}")
+
         pdf_to_xlsx(pdf_filename, xlsx_output_filename)
-        
-        
 
-    # for filename in os.listdir("output/Temp"):
-    #     if filename.endswith(".pdf"):
-    #         os.remove(f"output/Temp/{filename}")
+    for filename in os.listdir("output/Temp"):
+        if filename.endswith(".pdf"):
+            os.remove(f"output/Temp/{filename}")
 
-    # for filename in os.listdir("output"):
+    # for filename in os.listdir("output/Xlsx"):
     #     if filename.endswith(".xlsx"):
-    #         os.remove(f"output/{filename}")
+    #         os.remove(f"output/Xlsx/{filename}")
 
     print("Done :) ")
     pass
