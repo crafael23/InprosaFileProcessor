1f792559dac092f56cce09bc733453050012d278
Claudio Vasquez <claudio.vasquez@unah.hn>
2024-01-30 15:10:45 -0600

    terminado

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import camelot
 import os
 import concurrent.futures
 
+
 def split_pdf(archivo: str):
     with open(archivo, "rb") as pdf_file:
         pdf_reader = PyPDF2.PdfReader(pdf_file)
@@ -21,15 +22,13 @@ def split_pdf(archivo: str):
 
 
 def pdf_to_dataFrame(pdf_file_name):
-    
     tables = camelot.read_pdf(pdf_file_name, pages="all", flavor="stream")
 
     for i, table in enumerate(tables):  # type: ignore
         df = table.df
-        df = df.iloc[:,:5]
+        df = df.iloc[:, :5]
         rows_to_delete = []
         try:
-
             # Agarra los valores de las primeras 3 filas y los guarda en variables
             linea1 = df.iloc[0].values
             linea2 = df.iloc[1].values
@@ -98,20 +97,30 @@ def pdf_to_dataFrame(pdf_file_name):
                 df.iloc[0, 1] = df.iloc[0, 1] + " " + dato
                 df.drop(1, inplace=True)
                 df.reset_index(drop=True, inplace=True)
-            
+
             df.iloc[0, 2] = df.iloc[1, 1]
             df.iloc[0, 3] = 0
             df.iloc[0, 4] = 0
-        
+
             tipodeinsumo = df.iloc[2, 1]
-            
-            vocabulario_excepciones = [valor_CodigoActividad,"Unidad:","Tipo De Insumo:", "Codigo_Insumo", ""]
+
+            vocabulario_excepciones = [
+                valor_CodigoActividad,
+                "Unidad:",
+                "Tipo De Insumo:",
+                "Codigo_Insumo",
+                "",
+            ]
             # Revisa si la fila esta vacia y solo tiene un valor en el nombre del insumo, si es asi, lo agrega al nombree dle insumo anterior y elimina la fila que no tenia nada mas que eso
             last_column_number = len(df.columns)
-            
-            vocabulario_borrar = ["Tipo De Insumo:","Codigo_Insumo", "LICITACIONES\nUsuario:"]
-            
-            df[str(last_column_number)] = ''
+
+            vocabulario_borrar = [
+                "Tipo De Insumo:",
+                "Codigo_Insumo",
+                "LICITACIONES\nUsuario:",
+            ]
+
+            df[str(last_column_number)] = ""
             df.iloc[0, last_column_number] = "+"
             for index in range(len(df)):
                 if df.iloc[index, 0] == "Tipo De Insumo:":
@@ -126,27 +135,24 @@ def pdf_to_dataFrame(pdf_file_name):
                     text = df.iloc[index, 1]
                     df.iloc[index - 1, 1] = df.iloc[index - 1, 1] + " " + text
                     rows_to_delete.append(index)
-                
-                if (df.iloc[index,0] not in vocabulario_excepciones):
+
+                if df.iloc[index, 0] not in vocabulario_excepciones:
                     df.iloc[index, last_column_number] = tipodeinsumo
-                
-                if (df.iloc[index,0] in vocabulario_borrar):
+
+                if df.iloc[index, 0] in vocabulario_borrar:
                     rows_to_delete.append(index)
-                    
+
         except Exception as e:
             print("Error en el archivo: ", pdf_file_name)
             print("Error: ", e)
             print(df)
             return
-        
+
         rows_to_delete.append(1)
         df = df.drop(rows_to_delete)
         df.reset_index(drop=True, inplace=True)
 
-        # print("Tabla despues de procesar")
-        # print(df)
-        
-        # print("\n\n")
+
 
         return df
 
@@ -164,7 +170,7 @@ def merge(path: str, output: str):
     for file in xlsx_files:
         df = pd.read_excel(os.path.join(path, file))
         df_list.append(df)
-        
+
         empty_df = pd.DataFrame([[""] * len(df.columns)] * 2, columns=df.columns)
         df_list.append(empty_df)
     # Concatenate all the DataFrames together
@@ -172,64 +178,53 @@ def merge(path: str, output: str):
 
     # Write the merged DataFrame to a new Excel file
     merged_df.to_excel(output, index=False)
-    
+
 
 def process_file(file):
     dataframe = pdf_to_dataFrame(f"output/Temp/{file}")
     if dataframe is not None:
-        empty_df = pd.DataFrame([[""] * 5] * 2, columns=[f'Column{i+1}' for i in range(5)])
+        empty_df = pd.DataFrame(
+            [[""] * 5] * 2, columns=[f"Column{i+1}" for i in range(5)]
+        )
         return dataframe, empty_df
     return None, None
-    
-    
+
 
 def main():
-    # path = "Fichas_final.pdf"
-    # path2 = "Fichas1.pdf"
-    
-    # split_pdf(path)
-    # split_pdf(path2)
-    
-    # path = path.split(".")[0]
-    # path2 = path2.split(".")[0]
-  
+    path = "Fichas_final.pdf"
+    path2 = "Fichas1.pdf"
+
+    split_pdf(path)
+    split_pdf(path2)
 
     files = os.listdir("output/Temp")
     pdf_files = [file for file in files if file.endswith(".pdf")]
-    
-    
 
     df_list = []
 
     print("Converting pdf to dataframe")
-    for file in pdf_files:
-        dataframe = pdf_to_dataFrame(f"output/Temp/{file}")
-        if dataframe is not None:
-            df_list.append(dataframe)
-            empty_df = pd.DataFrame([[""] * 5] * 2, columns=[f'Column{i+1}' for i in range(5)])
-            df_list.append(empty_df)
-            
-   
+    with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
+        futures = [executor.submit(process_file, file) for file in pdf_files]
+        for future in concurrent.futures.as_completed(futures):
+            dataframe, empty_df = future.result()
+            if dataframe is not None:
+                df_list.append(dataframe)
+                df_list.append(empty_df)
 
-        print("Merging files")
-        merged_df = pd.concat(df_list, ignore_index=True)
-        
-        print("Writing to excel")
-        merged_df.to_excel("output/End/FinalFinal.xlsx", index=False,header=False)
-    
+    print("Merging files")
+    merged_df = pd.concat(df_list, ignore_index=True)
 
-        
+    print("Writing to excel")
+    merged_df.to_excel("output/End/FinalFinal.xlsx", index=False, header=False)
 
     print("Deleting temp files")
     for filename in os.listdir("output/Temp"):
         if filename.endswith(".pdf"):
             os.remove(f"output/Temp/{filename}")
 
-    
     # for filename in os.listdir("output/Xlsx"):
     #     if filename.endswith(".xlsx"):
     #         os.remove(f"output/Xlsx/{filename}")
-    
 
     print("Done :) ")
     pass
